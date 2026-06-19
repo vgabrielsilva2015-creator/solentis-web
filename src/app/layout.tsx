6eafb8945ff385dafc11eaec4a82741e4f3f9afa
Vitor <v.gabriel.silva2015@gmail.com>
2026-06-19 15:07:07 -0300

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CommandMenu } from "@/components/ui/command-menu";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         {children}
         <CommandMenu />
+        <Analytics />
       </body>
     </html>
   );
